84e0b7b6f4d03900ecea49f1511a9152b1d935b8
jarawd <devjara11@gmail.com>
2026-02-15 11:31:05 -0600

    add 2D form

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,18 @@
-import { formUnits, formValues, form } from './utils.js';
+import {
+  form1DUnits,
+  form1DValues,
+  form1D,
+  form2D,
+  form2DUnits,
+  form2DValues,
+} from './utils.js';
 
+// Funciones para los cálculos en 1D
 function excludingHolding() {
-  const lengthMaterialUnit = formUnits.materialLengthUnit.value;
-  const holdingWasteUnit = formUnits.holdingWasteUnit.value;
-  const materialLength = Number(formValues.materialLengthValue.value);
-  const holdingLength = Number(formValues.holdingWasteValue.value);
+  const lengthMaterialUnit = form1DUnits.materialLengthUnit.value;
+  const holdingWasteUnit = form1DUnits.holdingWasteUnit.value;
+  const materialLength = Number(form1DValues.materialLengthValue.value);
+  const holdingLength = Number(form1DValues.holdingWasteValue.value);
 
   const materialInches =
     lengthMaterialUnit === 'ft' ? materialLength * 12 : materialLength;
@@ -17,8 +25,8 @@ function excludingHolding() {
 }
 
 function excludingKnots() {
-  const knotWaste = formUnits.knotWasteUnit.value;
-  const knotLength = Number(formValues.knotWasteValue.value);
+  const knotWaste = form1DUnits.knotWasteUnit.value;
+  const knotLength = Number(form1DValues.knotWasteValue.value);
   const lengthInches = knotWaste === 'cm' ? knotLength / 2.54 : knotLength;
   const withoutHolding = excludingHolding();
   const finalLength = withoutHolding - lengthInches;
@@ -30,10 +38,10 @@ function round(num) {
 }
 
 function cutArea() {
-  const cutterUnit = formUnits.cutterWidthUnit.value;
-  const cutUnit = formUnits.cutWidthUnit.value;
-  const cutter = Number(formValues.cutterWidthValue.value);
-  const cut = Number(formValues.cutWidthValue.value);
+  const cutterUnit = form1DUnits.cutterWidthUnit.value;
+  const cutUnit = form1DUnits.cutWidthUnit.value;
+  const cutter = Number(form1DValues.cutterWidthValue.value);
+  const cut = Number(form1DValues.cutWidthValue.value);
 
   const cutterInches = cutterUnit === 'mm' ? cutter / 25.4 : cutter;
   const cutInches = cutUnit === 'mm' ? cut / 25.4 : cut;
@@ -42,11 +50,75 @@ function cutArea() {
   const freeArea = excludingKnots();
   const cutsQty = freeArea / sum;
 
-  const result = formValues.result;
+  const result = form1DValues.result;
   result.value = round(cutsQty);
 }
 
-form.addEventListener('submit', (e) => {
+form1D.addEventListener('submit', (e) => {
   e.preventDefault();
   cutArea();
+});
+
+// Funciones para los cálculos en 2D
+
+function getLength() {
+  const totalLengthUnits = form2DUnits.lengthUnit.value;
+  const totalLengthValue = Number(form2DValues.lengthValue.value);
+  const totalLength =
+    totalLengthUnits === 'cm' ? totalLengthValue / 2.54 : totalLengthValue;
+
+  const noCutUnitsX = form2DUnits.noCutXUnit.value;
+  const noCutValueX = Number(form2DValues.noCutXValue.value);
+  const noCutLength = noCutUnitsX === 'cm' ? noCutValueX / 2.54 : noCutValueX;
+
+  const finalCutLength = totalLength - noCutLength;
+  return finalCutLength;
+}
+
+function getWidth() {
+  const totalWidthUnits = form2DUnits.widthUnit.value;
+  const totalWidthValue = Number(form2DValues.widthValue.value);
+  const totalWidth =
+    totalWidthUnits === 'cm' ? totalWidthValue / 2.54 : totalWidthValue;
+
+  const noCutUnitsY = form2DUnits.noCutYUnit.value;
+  const noCutValueY = Number(form2DValues.noCutYValue.value);
+  const noCutWidth = noCutUnitsY === 'cm' ? noCutValueY / 2.54 : noCutValueY;
+
+  const finalCutWidth = totalWidth - noCutWidth;
+  return finalCutWidth;
+}
+
+function getAppDimension() {
+  const diameterUnit = form2DUnits.diameterUnit.value;
+  const diameterValue = Number(form2DValues.diameterValue.value);
+  const diameter = diameterUnit === 'cm' ? diameterValue / 2.54 : diameterValue;
+
+  const gapUnit = form2DUnits.gapUnit.value;
+  const gapValue = Number(form2DValues.gapValue.value);
+  const gap = gapUnit === 'cm' ? gapValue / 2.54 : gapValue;
+
+  const appDimension = diameter + gap;
+
+  return appDimension;
+}
+
+function getAppsQuantity() {
+  const length = getLength();
+  const width = getWidth();
+  const app = getAppDimension();
+
+  const appsX = length / app;
+  const appsY = width / app;
+
+  const totalApps = appsX * appsY;
+
+  const result = form2DValues.result;
+
+  result.value = round(totalApps);
+}
+
+form2D.addEventListener('submit', (e) => {
+  e.preventDefault();
+  getAppsQuantity();
 });
--- javascript/utils.js
+++ javascript/utils.js
@@ -1,8 +1,9 @@
 const d = document;
 
-export const form = d.querySelector('.parameters_form');
+// Elementos para los cálculos en 1D
+export const form1D = d.querySelector('.parameters_form-1D');
 
-export const formUnits = {
+export const form1DUnits = {
   materialLengthUnit: d.querySelector('.material-length_unit'),
   holdingWasteUnit: d.querySelector('.holding-waste_unit'),
   knotWasteUnit: d.querySelector('.knot-waste_unit'),
@@ -10,17 +11,35 @@ export const formUnits = {
   cutWidthUnit: d.querySelector('.cut-width_unit'),
 };
 
-export const formValues = {
+export const form1DValues = {
   materialLengthValue: d.querySelector('.material-length_value'),
   holdingWasteValue: d.querySelector('.holding-waste_value'),
   knotWasteValue: d.querySelector('.knot-waste_value'),
   cutterWidthValue: d.querySelector('.cutter-width_value'),
   cutWidthValue: d.querySelector('.cut-width_value'),
-  button: d.querySelector('.parameters_form__btn'),
-  result: d.querySelector('.parameters_form__result'),
+  button: d.querySelector('.parameters_form-1D__btn'),
+  result: d.querySelector('.parameters_form-1D__result'),
 };
 
-export const formResults = {
-  noHolding: d.querySelector('.no-holding'),
-  noKnots: d.querySelector('.no-knots'),
+// Elementos para los cálculos en 2D
+export const form2D = d.querySelector('.parameters_form-2D');
+
+export const form2DUnits = {
+  lengthUnit: d.querySelector('.length_unit'),
+  widthUnit: d.querySelector('.width_unit'),
+  noCutXUnit: d.querySelector('.no-cut-x_unit'),
+  noCutYUnit: d.querySelector('.no-cut-y_unit'),
+  diameterUnit: d.querySelector('.diameter_unit'),
+  gapUnit: d.querySelector('.gap_unit'),
+};
+
+export const form2DValues = {
+  lengthValue: d.querySelector('.length_value'),
+  widthValue: d.querySelector('.width_value'),
+  noCutXValue: d.querySelector('.no-cut-x_value'),
+  noCutYValue: d.querySelector('.no-cut-y_value'),
+  diameterValue: d.querySelector('.diameter_value'),
+  gapValue: d.querySelector('.gap_value'),
+  button: d.querySelector('.parameters_form-2D__btn'),
+  result: d.querySelector('.parameters_form-2D__result'),
 };
